3facdbd52cf9f23520ee57f0b7b707d4e82f8e9a
emt2cy <emt2cy@virginia.edu>
2015-06-29 15:24:30 +0200

    made cost controller

--- app/controllers/costs_controller.rb
+++ app/controllers/costs_controller.rb
@@ -1,1 +1,20 @@
-costs_controller.rb
+class CostsController < ApplicationController
+
+def create
+	@user = User.find(params[:user_id])
+	@cost = User.costs.create(cost_params)
+	redirect_to user_path(@user)
+end
+
+def destroy
+	@user = User.find(params[:user_id])
+	@cost = @user.costs.find(params[:id])
+	@cost.destroy
+	redirect_to user_path(@user)
+end
+
+private
+	def cost_params
+		params.require(:cost).permit(:iteam, :cost)
+	end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,3 @@
 class User < ActiveRecord::Base
+	has_many :costs, dependent: :destroy 
 end
